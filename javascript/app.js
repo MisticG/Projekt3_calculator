1d68f1716ce8bebbd9459964091d26fea6f30fdd
misticG <43235039+MisticG@users.noreply.github.com>
2022-11-03 18:58:07 +0100

    variables

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+const numberBtn = document.querySelectorAll('[number]');
+const operationBtn = document.querySelectorAll('[operation]');
+const equalsBtn = document.querySelector('[equals]');
+const deleteBtn = document.querySelector('[delete]');
+const allClearBtn = document.querySelector('[all-clear]');
+const prevOperandText = document.querySelector('[previous-operand]');
+const currOperandText = document.querySelector('[current-operand]');
+
